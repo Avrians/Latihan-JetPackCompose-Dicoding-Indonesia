af20652e1a4441a083ce2d67039c4d80d255cb85
Avrians <avriansyahb10@gmail.com>
2024-08-07 14:42:27 +0700

    [av][finish][Latihan AndroidView di dalam Composable]

--- LatihanInteroperability/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
+++ LatihanInteroperability/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
@@ -1,8 +1,8 @@
 package com.dicoding.newsapp.ui.detail
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -10,14 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -26,17 +19,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
-import androidx.core.content.IntentCompat
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.IntentCompat.getParcelableExtra
 import com.dicoding.newsapp.R
 import com.dicoding.newsapp.data.local.entity.NewsEntity
-import com.dicoding.newsapp.databinding.ActivityNewsDetailBinding
 import com.dicoding.newsapp.ui.ViewModelFactory
 
 class NewsDetailActivity : AppCompatActivity() {
 
     private lateinit var newsDetail: NewsEntity
-    private lateinit var binding: ActivityNewsDetailBinding
     private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
     private val viewModel: NewsDetailViewModel by viewModels {
         factory
@@ -44,65 +35,26 @@ class NewsDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        newsDetail = IntentCompat.getParcelableExtra(intent, NEWS_DATA, NewsEntity::class.java) as NewsEntity
+
+        newsDetail = getParcelableExtra(intent, NEWS_DATA, NewsEntity::class.java) as NewsEntity
 
         setContent {
-            MaterialTheme{
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NewsDetailScreen(newsDetail = newsDetail, viewModel = viewModel)
+                    NewsDetailScreen(
+                        newsDetail,
+                        viewModel
+                    )
                 }
             }
         }
-
-        
     }
 
     companion object {
         const val NEWS_DATA = "data"
-    }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun NewsDetailContent(
-    title: String,
-    url: String,
-    bookmarkStatus: Boolean,
-    updateBookmarkStatus: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Scaffold(
-        topBar = {
-         TopAppBar(title = { 
-            Text(
-                text = title,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-         },
-             actions = {
-                 IconButton(onClick = updateBookmarkStatus) {
-                     Icon(
-                         painter = if (bookmarkStatus) {
-                             painterResource(id = R.drawable.ic_bookmarked_white)
-                         } else {
-                             painterResource(id = R.drawable.ic_bookmark_white)
-                         },
-                         contentDescription = stringResource(R.string.save_bookmark))
-                 }
-             })   
-        },
-        modifier = modifier
-    ) { innerPadding ->
-        Box(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()){
-
-        }
     }
 }
 
@@ -117,19 +69,70 @@ fun NewsDetailScreen(
         newsDetail.title,
         newsDetail.url.toString(),
         bookmarkStatus,
-        updateBookmarkStatus = { viewModel.changeBookmark(newsDetail) }
+        updateBookmarkStatus = {
+            viewModel.changeBookmark(newsDetail)
+        }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsDetailContent(
+    title: String,
+    url: String,
+    bookmarkStatus: Boolean,
+    updateBookmarkStatus: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                actions = {
+                    IconButton(onClick = updateBookmarkStatus) {
+                        Icon(
+                            painter = if (bookmarkStatus) {
+                                painterResource(R.drawable.ic_bookmarked_white)
+                            } else {
+                                painterResource(R.drawable.ic_bookmark_white)
+                            },
+                            contentDescription = stringResource(R.string.save_bookmark),
+                        )
+                    }
+                }
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+            AndroidView(
+                factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        webViewClient = WebViewClient()
+                    }
+                },
+                update = {
+                    it.loadUrl(url)
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NewsDetailContentPreview() {
     MaterialTheme {
-        NewsDetailContent(
-            "News News",
-            "www.dicoding.com",
-                    false,
-            {}
-        )
+        NewsDetailContent("New News", "www.dicoding.com", false, {})
     }
 }
